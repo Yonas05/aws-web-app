b6dd1108ab40ff690690043205ac25930225aef0
Yonas Fayera <yonasfayera05@gmail.com>
2026-01-01 16:18:48 +0300

    terraform configuration

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  cloud {
+    organization = "yonas-fayera-itana-terraform-iac"
+    workspaces {
+      name = "aws-web-app"
+    }
+  }
+
+required_providers {
+  aws = {
+    source  = "hashicorp/aws"
+    version = "~> 4.0"
+  }
+}
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t3.micro"
+}
+
+
