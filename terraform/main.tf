2c7337e03e897f7c3e8616468db9515adcc9e8e7
Yonas Fayera <yonasfayera05@gmail.com>
2026-01-04 23:08:22 +0300

    private subnet my database application

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,7 +23,7 @@ provider "aws" {
 # -------------------------
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-  tags = { Name = "my-vpc" }
+  tags       = { Name = "my-vpc" }
 }
 
 # -------------------------
@@ -32,8 +32,16 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  tags = { Name = "public-subnet-1a" }
+  tags                    = { Name = "public-subnet-1a" }
   map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
+}
+
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  tags                    = { Name = "private-subnet-1a" }
+  map_public_ip_on_launch = false
   availability_zone       = "us-east-1a"
 }
 
